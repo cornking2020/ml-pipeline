1aa520fdc961d16b9aaadeccf7527e405fa9a64a
CORN-玉米王 <kuzoncby@gmail.com>
2023-03-16 02:00:28 +0700

    💾

--- dags/demo.py
+++ dags/demo.py
@@ -5,7 +5,7 @@ from airflow.decorators import task
 from airflow.operators.bash import BashOperator
 
 # A DAG represents a workflow, a collection of tasks
-with DAG(dag_id="demo", start_date=datetime(2023, 1, 1), schedule="*/1 * * * *") as dag:
+with DAG(dag_id="demo", start_date=datetime(2023, 1, 1), schedule="None") as dag:
     # Tasks are represented as operators
     hello = BashOperator(task_id="hello", bash_command="echo hello")
 
